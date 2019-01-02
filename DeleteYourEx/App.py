711b2e8da167ceb644c01d1871c7352c868bee1f
linyi <jianglinyi97@gmail.com>
2019-01-01 23:36:34 -0500

    Can show target image in a label

--- DeleteYourEx/App.py
+++ DeleteYourEx/App.py
@@ -1,6 +1,9 @@
 import sys
 import os
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5 import QtCore
+from PyQt5 import QtGui
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
 path = os.path.dirname(__file__) #uic paths from itself, not the active dir, so path needed
@@ -10,6 +13,9 @@ Ui_MainWindow, QtBaseClass = uic.loadUiType(path + qtCreatorFile) #process throu
 
 
 class MyApp(QMainWindow, Ui_MainWindow): #gui class
+    target_image = ""
+    unknown_images = []
+
     def __init__(self):
         #The following sets up the gui via Qt
         super(MyApp, self).__init__()
@@ -18,29 +24,33 @@ class MyApp(QMainWindow, Ui_MainWindow): #gui class
 
         self.ui.actionOpen_Folder.triggered.connect(self.openFolderDialog)
         self.ui.actionOpen_Files.triggered.connect(self.openFileNamesDialog)
+        self.ui.actionLoad_Target.triggered.connect(self.openFileNameDialog)
+
         #set up callbacks
 
     def openFileNamesDialog(self):
+        global unknown_images
         options = QFileDialog.Options()
 
         options |= QFileDialog.DontUseNativeDialog
         files, _ = QFileDialog.getOpenFileNames(self, "Select File", "",
                                                 "Images Files (*.png *.xpm *.jpg)", options=options)
         if files:
-            print(files)
-            return files
-        return
+            unknown_images = files
+            print(unknown_images)
 
     def openFileNameDialog(self):
+        global target_image
         options = QFileDialog.Options()
 
         options |= QFileDialog.DontUseNativeDialog
         file, _ = QFileDialog.getOpenFileName(self, "Select File", "",
                                               "Images Files (*.png *.xpm *.jpg)", options=options)
         if file:
-            print(file)
-            return file
-        return
+            target_image = file
+            pixmap = QtGui.QPixmap(file).scaled(300, 400, aspectRatioMode=QtCore.Qt.KeepAspectRatio,
+                                                transformMode=QtCore.Qt.SmoothTransformation)
+            self.ui.targetFace.setPixmap(pixmap)
 
     def openFolderDialog(self):
         folder_path = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
