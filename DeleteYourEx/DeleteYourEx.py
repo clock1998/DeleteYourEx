e4e8997ca73a18bdc9c544632917e4ad1a29ec25
linyi <jianglinyi97@gmail.com>
2019-01-07 22:46:59 -0500

    Finish Multiprocessing for filter out images

--- DeleteYourEx/DeleteYourEx.py
+++ DeleteYourEx/DeleteYourEx.py
@@ -1,35 +1,6 @@
 import face_recognition
 import os
-
-#
-# # Load the jpg files into numpy arrays
-# biden_image = face_recognition.load_image_file("biden.jpg")
-# obama_image = face_recognition.load_image_file("obama.jpg")
-# unknown_image = face_recognition.load_image_file("obama2.jpg")
-# known_faces = []
-# # Get the face encodings for each face in each image file
-# # Since there could be more than one face in each image, it returns a list of encodings.
-# # But since I know each image only has one face, I only care about the first encoding in each image, so I grab index 0.
-# try:
-#     biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-#     obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-#     unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
-#     known_faces = [
-#         biden_face_encoding,
-#         obama_face_encoding
-#     ]
-# except IndexError:
-#     print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
-#     quit()
-#
-#
-# # results is an array of True/False telling if the unknown face matched anyone in the known_faces array
-# results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
-#
-# print("Is the unknown face a picture of Biden? {}".format(results[0]))
-# print("Is the unknown face a picture of Obama? {}".format(results[1]))
-# print("Is the unknown face a new person that we've never seen before? {}".format(not True in results))
-
+import multiprocessing
 
 def remove_images_without_target_face(unknown_images_path, target_face_path):
     """
@@ -38,32 +9,44 @@ def remove_images_without_target_face(unknown_images_path, target_face_path):
     :param target_face_path: the target image path.
     :return: a list of images that has the target face.
     """
+    manager = multiprocessing.Manager()
+    p_return_values = manager.list()
+    jobs = []
     # gets the target face encoding.
     target_face_encoding = get_target_face_encodings(target_face_path)
     image_path_and_numpy_array = {}
     for i in unknown_images_path:
         image_path_and_numpy_array[i] = face_recognition.load_image_file(i)
     for i in image_path_and_numpy_array:
-        try:
-            face_encodings = face_recognition.face_encodings(image_path_and_numpy_array[i])
-            if len(face_encodings) > 1:
-                results = face_recognition.compare_faces(face_encodings, target_face_encoding)
-                counter = 0
-                for r in results:
-                    if r:
-                        counter += 1
-                if counter == 0:
-                    image_path_and_numpy_array[i] = None
-            else:
-                results = face_recognition.compare_faces(face_encodings, target_face_encoding)
-                if not results[0]:
-                    image_path_and_numpy_array[i] = None
-        except IndexError:
-            image_path_and_numpy_array[i] = None
+        p = multiprocessing.Process(target=recognize, args=(i, image_path_and_numpy_array, target_face_encoding,
+                                                            p_return_values))
+        jobs.append(p)
+        p.start()
+        # try:
+        #     face_encodings = face_recognition.face_encodings(image_path_and_numpy_array[i])
+        #     if len(face_encodings) > 1:
+        #         results = face_recognition.compare_faces(face_encodings, target_face_encoding)
+        #         counter = 0
+        #         for r in results:
+        #             if r:
+        #                 counter += 1
+        #         if counter == 0:
+        #             image_path_and_numpy_array[i] = None
+        #     else:
+        #         results = face_recognition.compare_faces(face_encodings, target_face_encoding)
+        #         if not results[0]:
+        #             image_path_and_numpy_array[i] = None
+        # except IndexError:
+        #     image_path_and_numpy_array[i] = None
     processed_images_list = []
+    for i in jobs:
+        i.join()
+
+    for i in p_return_values:
+        image_path_and_numpy_array.pop(i)
+
     for i in image_path_and_numpy_array:
-        if image_path_and_numpy_array[i] is not None:
-            processed_images_list.append(i)
+        processed_images_list.append(i)
     return processed_images_list
 
 
@@ -78,3 +61,22 @@ def delete_images(images):
             os.remove(i)
         else:
             print("The file does not exist")
+
+
+def recognize(i, image_path_and_numpy_array, target_face_encoding, p_return_values):
+    try:
+        face_encodings = face_recognition.face_encodings(image_path_and_numpy_array[i])
+        if len(face_encodings) > 1:
+            results = face_recognition.compare_faces(face_encodings, target_face_encoding)
+            counter = 0
+            for r in results:
+                if r:
+                    counter += 1
+            if counter == 0:
+                p_return_values.append(i)
+        else:
+            results = face_recognition.compare_faces(face_encodings, target_face_encoding)
+            if not results[0]:
+                p_return_values.append(i)
+    except IndexError:
+        p_return_values.append(i)
